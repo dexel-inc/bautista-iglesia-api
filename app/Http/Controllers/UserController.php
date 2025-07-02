9099338333908df56e312fa83f066944488f66b3
Anderson Cardona Ortiz <anderson.cardona@evertecinc.com>
2025-07-01 19:12:33 -0500

    feat: add logic on users controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Actions\StoreUserAction;
+use App\Actions\UpdateUserAction;
+use App\Http\Requests\Users\StoreUserRequest;
+use App\Http\Requests\Users\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
-    public function store()
+    public function index(): array
     {
+        return User::all()->toArray();
+    }
 
+    public function store(StoreUserRequest $request, StoreUserAction $action): JsonResponse
+    {
+        $action->execute($request->validated());
+
+        return response()->json([
+            'user' => $request->validated(),
+            'message' => 'The user was created correctly',
+        ]);
+    }
+
+    public function update(UpdateUserRequest $request, User $user, UpdateUserAction $action): JsonResponse
+    {
+        $action->execute($request->validated(), $user);
+
+        return response()->json([
+            'user' => $request->validated(),
+            'message' => 'The user was created correctly',
+        ]);
+    }
+
+    public function show(User $user): User
+    {
+        return $user;
+    }
+
+    public function destroy(User $user): JsonResponse
+    {
+        $user->delete();
+
+        return response()->json([
+            'user deleted' => $user,
+            'message' => 'The user was deleted correctly',
+        ]);
     }
 }
