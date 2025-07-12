322f2ce8cb7853c42ddb8e86a6602d1ae192561d
Anderson Cardona Ortiz <anderson.cardona@evertecinc.com>
2025-07-11 21:12:30 -0500

    feat: create route for testimonies

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\TestimonyController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
 Route::apiResource('subscriptions', SubscriptionController::class);
+Route::apiResource('testimonies', TestimonyController::class);
