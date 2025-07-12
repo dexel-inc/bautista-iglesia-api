fb684f6b37e087bddb12749e20b0a6d24126ddb2
Anderson Cardona Ortiz <anderson.cardona@evertecinc.com>
2025-07-11 21:02:16 -0500

    feat: add route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('subscriptions', SubscriptionController::class);
