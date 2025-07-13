48fe114ec0d800e5812fc944feb1e7c009cb413a
alejociro <alejandro.cciro@udea.edu.co>
2025-07-13 18:30:39 -0500

    refactor: delete unnecessary rule validation

--- app/Http/Requests/Users/RegisterRequest.php
+++ app/Http/Requests/Users/RegisterRequest.php
@@ -18,7 +18,7 @@ class RegisterRequest extends FormRequest
             'surname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'phone' => ['required', 'string', 'max:20', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8'],
         ];
     }
 }
