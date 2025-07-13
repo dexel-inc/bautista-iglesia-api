40e37001b720cf24403a204546d65084919eaf8e
Anderson Cardona Ortiz <anderson.cardona@evertecinc.com>
2025-07-12 23:05:10 -0500

    feat: create test for enabled and disabled subscription

---
+++ tests/Feature/Subscriptions/SubscriptionToggleTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature\Subscriptions;
+
+use App\Constants\Response;
+use App\Constants\Status;
+use App\Models\Subscription;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SubscriptionToggleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_disables_enabled_subscription(): void
+    {
+        $subscription = Subscription::factory()->create([
+            'name' => 'Juan Pérez',
+            'email' => 'juan.perez@example.com',
+            'phone' => '1234567890',
+            'disabled_at' => null,
+        ]);
+
+        $response = $this->patchJson(route('subscriptions.toggle', $subscription));
+
+        $response->assertOk()
+            ->assertJson([
+                'body' => [
+                    'status' => Status::OK,
+                    'reason' => Response::HTTP_OK,
+                    'message' => 'The subscription was disabled correctly',
+                ],
+            ]);
+
+        $this->assertDatabaseMissing('subscriptions', [
+            'id' => $subscription->id,
+            'disabled_at' => null,
+        ]);
+    }
+
+    public function test_it_enables_disabled_subscription(): void
+    {
+        $subscription = Subscription::factory()->create([
+            'name' => 'Juan Pérez',
+            'email' => 'juan.perez@example.com',
+            'phone' => '1234567890',
+            'disabled_at' => now(),
+        ]);
+
+        $response = $this->patchJson(route('subscriptions.toggle', $subscription));
+
+        $response->assertOk()
+            ->assertJson([
+                'body' => [
+                    'status' => Status::OK,
+                    'reason' => Response::HTTP_OK,
+                    'message' => 'The subscription was enabled correctly',
+                ],
+            ]);
+
+        $this->assertDatabaseHas('subscriptions', [
+            'id' => $subscription->id,
+            'disabled_at' => null,
+        ]);
+    }
+
+    public function test_it_returns_404_for_nonexistent_subscription(): void
+    {
+        $response = $this->patchJson(route('subscriptions.toggle', 999));
+
+        $response->assertNotFound();
+    }
+} 
